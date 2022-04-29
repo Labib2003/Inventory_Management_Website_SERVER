8760fed5b82c8ebd4fe160b0bd539b420f6cd395
unknown <labib.salimi30@gmail.com>
2022-04-29 07:41:53 +0600

    mongodb connected

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+// mongodb connection
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dkxwe.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const productCollection = client.db('cricket-freak-warehouse-database').collection('products');
+
+        // get all API
+        app.get('/products', async (req, res) => {
+            const cursor = productCollection.find({});
+            const products = await cursor.toArray();
+            res.send(products);
+        })
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir);
 
 // https://floating-retreat-93986.herokuapp.com/
 app.get('/', (req, res) => {
